e6b7e3c5509e91dcc7b5afe3fc775aad6ab87cca
Victoria <doodo@gmail.com>
2021-12-06 21:29:24 +0300

    ineff solution is ready

--- ege_27/main.cpp
+++ ege_27/main.cpp
@@ -8,17 +8,24 @@ void div_30()
      +1) считать и запомнить числа
      +2) вывести все пары чисел
      +3) вместо пар вывести произведения
-     4) проверить делимость произведений
-     5) посчиттать и выести коичество подходящих пар
+     +4) проверить делимость произведений
+     +5) посчиттать и выести коичество подходящих пар
     */
    int n(0);
     cin >> n;
     vector<int> numbers(n, 0);
     for (int i(0); i < n; i++)
         cin >> numbers[i];
+
+    int good_pairs = 0;
     for (int j = 0; j < n; ++j)
         for (int i = j + 1; i < n; ++i)
-            cout << numbers[j] * numbers[i] << endl;
+            {
+                int cur_prod = numbers[j] * numbers[i];
+                if (cur_prod % 55 == 0)
+                    good_pairs++;
+            }
+    cout << good_pairs;
 }
 
 int main()

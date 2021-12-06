cae513be210f12731d5ae5c0c4bff54ec88e6553
Victoria <doodo@gmail.com>
2021-12-06 21:25:39 +0300

    reads and shows numbers

--- ege_27/main.cpp
+++ ege_27/main.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
-void div_15()
+void div_30()
 {
     /**
-     1) считать и запомнить числа
+     +1) считать и запомнить числа
      2) вывести все пары чисел
      3) вместо пар вывести произведения
      4) проверить делимость произведений
      5) посчиттать и выести коичество подходящих пар
     */
+   int n(0);
+    cin >> n;
+    vector<int> numbers(n, 0);
+    for (int i(0); i < n; i++)
+        cin >> numbers[i];
+    for (int i(0); i<n; i++)
+        cout << numbers[i] << " ";
 }
 
 int main()
 {
-    div_15;
+    div_30();
     return 0;
 }

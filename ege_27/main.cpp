f726f3c62bd706fdede7ef7b1d1c3695850d0d66
Victoria <doodo@gmail.com>
2021-12-07 01:25:47 +0300

    added development plan

--- ege_27/main.cpp
+++ ege_27/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
+void div_15()
+{
+    /**
+     1) считать и запомнить числа
+     2) найти наибольшее четное и наибольшее нечетное числа
+     3) вывести сумму наибольших чисел
+    */
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    div_15;
     return 0;
 }

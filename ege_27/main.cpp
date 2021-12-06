57b381aa490e491fabbf9159be2c9fc7b3908286
Victoria <doodo@gmail.com>
2021-12-07 01:05:42 +0300

    shows all pairs

--- ege_27/main.cpp
+++ ege_27/main.cpp
@@ -6,20 +6,22 @@ using namespace std;
 void max_sum_div()
 {
     /**
-    1)считать и сохранить все числа
-    2) вывести все пары
+    +1)считать и сохранить все числа
+    +2) вывести все пары
     3) вместо пары вывести суммы
     4) проверить делимость суммы
     5) вывести только наибольшую сумму
     */
-      int n(0);
+    int n(0);
     cin >> n;
     vector <int> numbers(n);
     for (int i(0); i<n; i++)
             cin>> numbers[i];
     cout << endl;
-    for (int i(0); i<n; i++)
-        cout << numbers[i] << " ";
+    for (int j(0); j<n-1; j++)
+        for (int i(j+1); i<n; i++)
+            cout << numbers[j]<<" "<<numbers[i]<< endl;
+
 
 }
 int main()

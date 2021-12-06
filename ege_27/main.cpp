dfaa8bb0c3188933f36a90fbaa3a79a3a023c7e6
Victoria <doodo@gmail.com>
2021-12-06 16:41:09 +0300

    effective solution is ready

--- ege_27/main.cpp
+++ ege_27/main.cpp
@@ -12,7 +12,7 @@ void div_6()
      делится на 2 НЕ на 3
      делится на 3 НЕ на 2
      не делится ни на 2 ни на 3
-     3) ответ n6 * n2 + n6 * n3 + n6 * no + n6*(n6-1) / 2 * n2 * n3
+     +3) ответ n6 * n2 + n6 * n3 + n6 * no + n6*(n6-1) / 2 * n2 * n3
     */ 
 
    int n6 = 0, n3 = 0, n2 = 0, no = 0;
@@ -21,17 +21,16 @@ void div_6()
     for (int i = 0; i<n; i++)
     {
         cin >> cur;
-        
         if (cur % 6 == 0) 
             n6++;
-        else if (cur % 2 ==0) 
+        else if (cur % 2 ==0 and cur % 3 != 0) 
             n2++;
-        else if (cur % 3 == 0) 
+        else if (cur % 3 == 0 and cur % 2 != 0) 
             n3++;
     }
 
     no = n - n6 - n3 - n2;
-    cout << n6 << ' ' << n3 << ' ' << n2 << ' ' <<  no;
+    cout << n6 * n2 + n6 * n3 + n6 * no + n6*(n6-1) / 2 + n2 * n3;
 
     
 }

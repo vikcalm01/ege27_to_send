f3ff219d5f0859b0e3e45f81768ee87b31a4f93f
Victoria <doodo@gmail.com>
2021-12-06 14:05:43 +0300

    shows all pairs

--- ege_27/main.cpp
+++ ege_27/main.cpp
@@ -16,8 +16,9 @@ void div_15()
     vector<int> numbers(n, 0);
     for (int i(0); i < n; i++)
         cin >> numbers[i];
-    for (int i(0); i<n; i++)
-        cout << numbers[i] << " ";
+    for (int j = 0; j < n; ++j)
+        for (int i = j + 1; i < n; ++i)
+            cout << numbers[j] << ' ' << numbers[i] << endl;
 }
 
 int main()

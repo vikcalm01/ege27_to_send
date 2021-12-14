e12b800c01528c9546e517c9270a8e8145fcdff8
Victoria <doodo@gmail.com>
2021-12-14 16:05:48 +0300

    fill array with 0

--- ege_27/main.cpp
+++ ege_27/main.cpp
@@ -9,9 +9,21 @@ void max_sum_div_eff_2()
      2) затем заполнить массив с счетчиками [x > 40][x % 40] += 1
      3) суммировать комплементарные остатки для 40 отдельно, 20 отдельно, остальных отдельно
     */
+    int x = 0;
+    int counters[2][40];
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 40; j++)
+        {
+            counters[i][j] = 0;
+        }
+    }
+
+    std::cin >> x;
 }
 int main()
 {
-    cout << "Hello world!" << endl;
+    max_sum_div_eff_2();
     return 0;
 }

d49b9d2a46b09fff50e6b0d3f94cad722b6cc6ad
Victoria <doodo@gmail.com>
2021-12-14 16:17:51 +0300

    effective solution is ready!

--- ege_27/main.cpp
+++ ege_27/main.cpp
@@ -6,7 +6,7 @@ void max_sum_div_eff_2()
 {
     /**
      +1) заполнить массив 2 на 40 нулями
-     2) затем заполнить массив с счетчиками [x > 40][x % 40] += 1
+     +2) затем заполнить массив с счетчиками [x > 40][x % 40] += 1
      3) суммировать комплементарные остатки для 40 отдельно, 20 отдельно, остальных отдельно
     */
     int x = 0;
@@ -19,16 +19,46 @@ void max_sum_div_eff_2()
             counters[i][j] = 0;
         }
     }
-    cin >> x;
+    cin>> x;
 
     while (x != -9999)
     {
         counters[x > 40][x % 40] += 1;
-        cin >> x;
+        cin>> x;
     }
+
+    int sum = 0;
+
+    int bigger_40 = counters[1][0], smaller_40 = counters[0][0];
+
+    sum += bigger_40 * (bigger_40 - 1) / 2;
+    sum += bigger_40 * smaller_40;
+
+    bigger_40 = counters[1][20];
+    smaller_40 = counters[0][20];
+
+    sum += bigger_40 * (bigger_40 - 1) / 2;
+    sum += bigger_40 * smaller_40;
+
+    for (int i = 1; i < 20; ++i)
+    {
+        int count_complimentary_num_bigger_40 = counters[1][40 - i];
+        bigger_40 = counters[1][i];
+        
+        int count_complimentary_num_smaller_40 = counters[0][40 - i];
+        smaller_40 = counters[0][i];
+        
+
+        sum += bigger_40 * count_complimentary_num_bigger_40;
+        sum += bigger_40 * count_complimentary_num_smaller_40;
+        sum += smaller_40 * count_complimentary_num_bigger_40;
+    }
+
+    cout << sum;
 }
+
 int main()
 {
     max_sum_div_eff_2();
-    return 0;
+    return 0 ;
 }

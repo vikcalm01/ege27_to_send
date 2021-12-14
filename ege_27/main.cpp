c338e7e434977e27bd741def1140981985cd0394
Victoria <doodo@gmail.com>
2021-12-14 13:09:58 +0300

    reads and shows numbers

--- ege_27/main.cpp
+++ ege_27/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
 void div_15()
 {
     /**
@@ -8,10 +9,19 @@ void div_15()
      2) найти наибольшее четное и наибольшее нечетное числа
      3) вывести сумму наибольших чисел
     */
+     int n(0);
+    cin >> n;
+    vector <int> numbers(n);
+    for (int i(0); i<n; i++)
+            cin>> numbers[i];
+    cout << endl;
+    for (int i(0); i<n; i++)
+        cout << numbers[i] << " ";
+
 }
 
 int main()
 {
-    div_15;
+    div_15();
     return 0;
 }

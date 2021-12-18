92fe958a1a0b080b8ff1d12951696a2217ab9307
Victoria <doodo@gmail.com>
2021-12-18 18:18:21 +0300

    shows numbers by first condition

--- ege_27/main.cpp
+++ ege_27/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-void num_sum_min_distance_ineff();
+void num_sum_min_distance_ineff()
 {
     /**
      1)считать последовательность чисел
@@ -10,6 +11,18 @@ void num_sum_min_distance_ineff();
      3) вывести пары удовлетворяющие второму условию
      4) вывести ответ
     */
+   int n;
+   cin >> n;
+   vector<int> numbers(n, 0);
+    for (int i(0); i < n; i++)
+        cin >> numbers[i];
+    
+   for (int i = 0; i < n; ++i)
+    for (int j = i + 1; j < n; ++j)
+    {
+        if (abs(i - j) > 6)
+            cout << numbers[i] << ' ' << numbers[j] << endl;
+    }
 }
 int main()
 {

3f0b490d2d94e3a5f6e2dd77cfb4917412d1b728
Victoria <vikcalm01@gmail.com>
2021-12-21 10:52:21 +0300

    shows sum of triple that min and can be devided by 4

--- ege_27/main.cpp
+++ ege_27/main.cpp
@@ -10,7 +10,7 @@ void A_course_173()
     +3) вывод пар
     +4) вывод троек
     +5) вывод подходящих троек
-    6) подсчёт подходящих троек
+    +6) подсчёт подходящих троек
 6
 5
 4
@@ -18,8 +18,13 @@ void A_course_173()
 2
 8
 7
+5 4 3 =  12
+5 4 7 = 16
+5 3 8=  16
+5 8 7= 20
+3 2 7=12
     */
-    int n(0), summa = 0;
+    int n(0), summa = 0, min_summa = 4000;
     cin >> n;
     vector<int> numbers(n, 0);
     for (int i(0); i < n; i++)
@@ -28,8 +33,14 @@ void A_course_173()
     for (int i(0); i < n; i++)
         for (int j(i+1); j<n; j++)
             for (int k(j+1); k < n; ++k)
-                if ((numbers[i] + numbers[j] + numbers[k]) % 4 == 0)
-                    cout << numbers[i]<<" "<<numbers[j] << " "<<numbers[k] << endl;
+            {
+                summa = numbers[i] + numbers[j] + numbers[k];
+                if (summa % 4 == 0 and summa < min_summa)
+                    min_summa = summa;
+                    //cout << numbers[i]<<" "<<numbers[j] << " "<<numbers[k] << endl;
+            }
+            cout << min_summa;
+
 
 }
 int main()

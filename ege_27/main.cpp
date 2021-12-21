1c660d1588a46f2f2e300eaf4cbbcb0bb27b52d0
Victoria <vikcalm01@gmail.com>
2021-12-21 11:00:30 +0300

    reads numbers

--- ege_27/main.cpp
+++ ege_27/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 void B_test_course_173()
@@ -9,6 +10,11 @@ void B_test_course_173()
     3) поиск 3 минимальных чисел
     4) сумма этих трех
     */
+    int n(0);
+    cin >> n;
+    vector<int> numbers(n, 0);
+    for (int i(0); i < n; i++)
+        cin >> numbers[i];
 }
 int main()
 {

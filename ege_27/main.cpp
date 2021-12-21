95d1f81dd35f0eb376c3e8296cbaa08bdf04763f
Victoria <vikcalm01@gmail.com>
2021-12-21 10:59:37 +0300

    added development plan + gitignore

--- ege_27/main.cpp
+++ ege_27/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
 using namespace std;
-
+void B_test_course_173()
+{
+    /**
+    1) ввод чисел
+    2) вывод чисел
+    3) поиск 3 минимальных чисел
+    4) сумма этих трех
+    */
+}
 int main()
 {
-    cout << "Hello world!" << endl;
+    B_test_course_173();
     return 0;
 }

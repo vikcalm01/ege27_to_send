5908489dd629bc7efdddcd0ec6f4136de07d67eb
Victoria <vikcalm01@gmail.com>
2021-12-21 10:31:55 +0300

    added a development plan

--- ege_27/main.cpp
+++ ege_27/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 
 using namespace std;
+void A_course_173()
+{
+    /**
+    1) ввод чисел
+    2) вывод чисел
+    3) вывод пар
+    4) вывод троек
+    5) вывод подходящих троек
+    6) подсчёт подходящих троек
+    */
 
+
+}
 int main()
 {
-    cout << "Hello world!" << endl;
+    A_course_173();
     return 0;
 }

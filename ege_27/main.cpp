bb3401d8e1ae847b6fda2497d78d6289f624d62a
Victoria <vikcalm01@gmail.com>
2021-12-21 11:49:12 +0300

    effective solution is ready

--- ege_27/main.cpp
+++ ege_27/main.cpp
@@ -5,19 +5,39 @@ using namespace std;
 void B_test_course_173()
 {
     /**
-    1) ввод чисел
-    2) вывод чисел
-    3) поиск 3 минимальных чисел
-    4) сумма этих трех
+    +1) ввод чисел
+    +2) вывод чисел
+    +3) поиск 3 минимальных чисел
+    +4) сумма этих трех
     */
-    int n(0);
+    int n(0), min_1 = 2000, min_2 = 2000, min_3 = 2000, c =0, summa = 0;
     cin >> n;
     vector<int> numbers(n, 0);
     for (int i(0); i < n; i++)
         {
             cin >> numbers[i];
-            cout << numbers[i] << ' ';
+            if (numbers[i] < min_1 and numbers[i] < min_2 and numbers[i] < min_3)
+                {
+                    c = min_1;
+                    min_1 = numbers[i];
+                    min_3 = min_2;
+                    min_2 = c;
+                }
+            else if (numbers[i] < min_2 and numbers[i] < min_3)
+            {
+                c = min_2;
+                min_2 = numbers[i];
+                min_3 = c;
+            }
+            else if (numbers[i] < min_3)
+            {
+                min_3 = numbers[i];
+            }
+            if ((min_1 + min_2 + min_3) % 4 == 0)
+                summa = min_1 + min_2 + min_3;
         }
+        cout << summa;
+
 }
 int main()
 {
